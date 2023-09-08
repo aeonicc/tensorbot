699c82dd9075914cf218fecc429f0c1ec66b6711
Vinicius de Moraes Nascimento <38895221+aeonicc@users.noreply.github.com>
2023-09-08 13:13:11 +0100

    2

--- hexy.py
+++ hexy.py
@@ -1,18 +1,20 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(4, GPIO.OUT)
+pin = 4
 
-pwm = GPIO.PWM(4, 50)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(pin, GPIO.OUT)
+
+pwm = GPIO.PWM(pin, 50)
 pwm.start(0)
 
 def set_angle(angle):
     duty = angle / 18 + 2
-    GPIO.output(4, True)
+    GPIO.output(pin, True)
     pwm.ChangeDutyCycle(duty)
     time.sleep(1)
-    GPIO.output(4, False)
+    GPIO.output(pin, False)
     pwm.ChangeDutyCycle(0)
 
-set_angle(45)
+set_angle(90)
